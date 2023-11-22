291e207c52f3150084779d1446b4a1a0a8faacb4
greg-1020 <greg.1020@kakaomobility.com>
2023-11-22 17:29:13 +0900

    impletments /parking/summary service api

--- api/chatgpt_adapter.py
+++ api/chatgpt_adapter.py
@@ -1,21 +1,25 @@
 import os
 import openai
 from dotenv import load_dotenv
+
 load_dotenv()
 
 openai.api_type = "azure"
 openai.api_version = "2023-06-01-preview"
 
-API_KEY = os.getenv("OPENAI_API_KEY","").strip()
+API_KEY = os.getenv("OPENAI_API_KEY", "").strip()
 assert API_KEY, "ERROR: Azure OpenAI Key is missing"
 openai.api_key = API_KEY
 
-RESOURCE_ENDPOINT = os.getenv("OPENAI_API_BASE","").strip()
+RESOURCE_ENDPOINT = os.getenv("OPENAI_API_BASE", "").strip()
 assert RESOURCE_ENDPOINT, "ERROR: Azure OpenAI Endpoint is missing"
 assert "openai.azure.com" in RESOURCE_ENDPOINT.lower(), "ERROR: Azure OpenAI Endpoint should be in the form: \n\n\t<your unique endpoint identifier>.openai.azure.com"
 openai.api_base = RESOURCE_ENDPOINT
 
 model = "gpt-35-turbo"
+deployment_default = "gpt-35-turbo-16k"
+deployment_gpt4 = "gpt-4"
+
 
 def request(text_prompt: str):
     return openai.ChatCompletion.create(
@@ -25,3 +29,105 @@ def request(text_prompt: str):
             {"role": "user", "content": text_prompt},
         ]
     )
+
+
+def get_parking_summary(data):
+    messages = [
+        {
+            "role": "system",
+            "content": """
+                You are assistant of parking service. You only response result as json.
+                Please do not include ```json ``` either.
+                
+                Dataset contains content, score, provider_name, and name in csv format.                
+                content is a subjective expression, score is a user evaluation score, and provider_name is the parking lot name.
+                
+                Group by provider_name, then extract words from content and summarize frequently used keywords.
+                Extract only meaningful words from other users when using the parking lot.
+                You need to analyze whether the content is positive or negative.
+                Provide positive words as positive_keywords and words with negative meaning as negative_keywords.                
+                Please provide a maximum of 5 each positive/negative word.                
+                
+                The json example is:
+                [
+                    {                        
+                        "provider_name": "주차장",                        
+                        "positive_keywords": ["넓다"]
+                        "negative_keywords": ["좁다", "비싸다"]
+                    }
+                ]                            
+            """
+        },
+        {
+            "role": "user",
+            "content": ("""
+                아래 csv 데이터에서 다른 사용자에게 의미있는 요약 정보 알려줘.
+                %s
+            """) % (data.to_string)
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        deployment_id=deployment_gpt4,
+        messages=messages,
+        temperature=0,
+        max_tokens=4096
+    )
+
+    # print(response)
+    print(response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
+
+
+def get_parking_suggest(model_name: str = ""):
+    messages = [
+        {
+            "role": "system",
+            "content": """
+                You are assistant of parking service. You only response result as json.
+                Dataset contains content, score, provider_name, and name.
+
+                First filter data by name matches input model name.
+
+                Then you suggest provider_name from filterd dataset by positive content and higher score.
+                The score is from 1 to 5 and 5 is max.
+                And the suggestion is effected by content more.
+
+                And find most positive content from filtered dataset as reason.
+
+                Expansive is negative.
+
+                The json example is
+                {
+                    "model_name": "카니발",
+                    "provider_name": "주차장",
+                    "reason": "넓다"
+                }
+
+                If you cannot found the provider_name sample:
+                {
+                    "model_name": "카니발",
+                    "provider_name": null,
+                    "reason": "데이터가 없음"
+                }
+            """
+        },
+        {
+            "role": "user",
+            "content": ("""
+                아래 데이터에서 %s인 차량이 제일 선호하는 주차장 1개만 그리고 이유를 알려줘
+                %s
+            """) % (model_name, model_name)
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        deployment_id=deployment_default,
+        messages=messages,
+        temperature=0,
+        max_tokens=10000
+    )
+
+    # print(response)
+    # print(response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import chatgpt_adapter
-
+import pandas as pd
 import json
 
 app = FastAPI()
@@ -13,12 +13,29 @@ def hello():
 
 @app.get("/parking/summary")
 def get_parking_summary():
-    with open("summary.json", 'r', encoding='utf-8') as file:
+    with open("data/parking_review_summary.json", 'r', encoding='utf-8') as file:
         summary = json.load(file)
-        return {"summary": summary}
+        suggest = chatgpt_adapter.get_parking_suggest("아반떼")
+        return {"summary": summary, "suggest": json.loads(suggest)}
+
+
+@app.post("/parking/summary")
+def get_parking_summary():
+    # TODO: 기능 개선
+    # 1. raw_data 디렉토리에서 파일을 찾는다.
+    # 2. 이미 json 파일이 만들어졌으면 스킵한다. 없으면 chatgpt에 요청해서 요약정보를 저장한다.
+    # 3. rate limit를 회피하기 위해 1회 요청 이후 약 30초 정도 쉬었다가 이어서 시도한다.
+    # 4. 디렉토리에서 json 파일을 모두 찾아서 합친다.
+    filename = "data/주차평가5점주관식-하이파킹 판교알파돔타워"
+    data = pd.read_csv(filename + ".csv")
+
+    summary = chatgpt_adapter.get_parking_summary(data)
+    summary_json = json.loads(summary)
+    with open(filename + ".json", "w") as file:
+        json.dump(summary_json, file, ensure_ascii=False)
 
 
 @app.get("/openai/talk")
-def get_parking_summary():
+def talk_chatgpt():
     text_prompt = "Should oxford commas always be used?"
     return chatgpt_adapter.request(text_prompt)
